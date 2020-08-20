f9f3442ad22306053582983a2f85ed7613f693bb
itsmedexter <deeside@pacbell.net>
2020-08-19 17:46:53 -0700

    added slice and replace method arguments

--- methods.js
+++ methods.js
@@ -12,11 +12,11 @@ indexOf()
 // sample: "catdog".indexOf('dog') = 3 --- starting letter of string
 // sample: 'catdog'.indexof(d) = 3
 
-slice()
+slice(), slice(beginIndex[, endIndex optional] )
 // sample: 'catdog'.slice(3) = 'dog'
 // takes a piece of the string from where you indicate index 
 // also start and stop arguments: 'superhero'.slice(0,5) = 'super'
 
-replace()
+replace(), replace(value1, value2)
 // what you want to replace, then what you want to replace it with
 // sample: 'baseball is fun'.replace('fun', 'boring') = 'baseball is boring'
