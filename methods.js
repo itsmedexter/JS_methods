5181d3c1304a52746408fdd5c7171a53d1a66b98
itsmedexter <deeside@pacbell.net>
2020-08-19 17:42:26 -0700

    few string methods

---
+++ methods.js
@@ -0,0 +1,22 @@
+toUpperCase()
+// upper case all letters in a string
+
+toLowerCase()
+// lowers all letters in a string
+
+trim()
+// removes spaces in front and after a string
+// sample: before: '     string     ', after: 'string'
+
+indexOf()
+// sample: "catdog".indexOf('dog') = 3 --- starting letter of string
+// sample: 'catdog'.indexof(d) = 3
+
+slice()
+// sample: 'catdog'.slice(3) = 'dog'
+// takes a piece of the string from where you indicate index 
+// also start and stop arguments: 'superhero'.slice(0,5) = 'super'
+
+replace()
+// what you want to replace, then what you want to replace it with
+// sample: 'baseball is fun'.replace('fun', 'boring') = 'baseball is boring'
